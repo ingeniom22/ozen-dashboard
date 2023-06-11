1e49c4ce42254f8229dcb80dc8847b0bf84f4886
ingeniom22 <jamesmichael0444@gmail.com>
2023-06-11 18:11:43 +0700

    add csrf_protect to views

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 @login_required
+@csrf_protect
 def index(request):
     # Page from the theme
     return render(request, "pages/dashboard.html")
