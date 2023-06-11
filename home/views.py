f84d8c0871e25deb38cfa1e1f4185f56837503a6
ingeniom22 <jamesmichael0444@gmail.com>
2023-06-11 18:24:28 +0700

    set views to csrf exempt (last resort)

--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @login_required
-@csrf_protect
+@csrf_exempt
 def index(request):
     # Page from the theme
     return render(request, "pages/dashboard.html")
